139e0eaf92a13c2fa05bc088f294378f99be28ae
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-03-01 07:30:26 -0500

    fix: tourDetail styling

--- app/views/_tourDetailView.js
+++ app/views/_tourDetailView.js
@@ -1,39 +1,7 @@
-// const requestedtour = {
-//   id: 2,
-//   name: "U.S.A",
-//   price: 1000,
-//   imagePath: "./images/tours/",
-//   imageSmall: "usa-small.png",
-//   imageLarge: "usa-large.png",
-//   image01: "usa-tour-01.png",
-//   image02: "usa-tour-02.png",
-//   image03: "usa-tour-03.png",
-//   day01: `<p>
-//   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-//   accusantium doloremque laudantium, totam rem aperiam, eaque
-//   ipsa quae ab illo inventore veritatis et quasi architecto
-//   beatae vitae dicta sunt explicabo.
-// </p>`,
-
-//   day02: `<p>
-//   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-//   accusantium doloremque laudantium, totam rem aperiam, eaque
-//   ipsa quae ab illo inventore veritatis et quasi architecto
-//   beatae vitae dicta sunt explicabo.
-// </p>`,
-
-//   day03: `<p>
-//   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-//   accusantium doloremque laudantium, totam rem aperiam, eaque
-//   ipsa quae ab illo inventore veritatis et quasi architecto
-//   beatae vitae dicta sunt explicabo.
-// </p>`,
-// };
-
 export const tourDetailView = (tour) =>
   // console.log("detailview:", tour.id);
   `        <section class="tour">
-<section class="banner-large">
+<section class="banner-large content-width">
   <img src="${tour.imagePath}${tour.imageLarge}" alt="" />
   <div class="banner-text">
     <h1 class="font-size-60 figure-title">${tour.name} tour package:</h1>
@@ -42,12 +10,12 @@ export const tourDetailView = (tour) =>
     </div>
   </div>
 </section>
-<section class="tour-tour.images">
+<section class="tour-images content-width">
   <img src="${tour.imagePath}${tour.image01}" alt="" />
   <img src="${tour.imagePath}${tour.image02}" alt="" />
   <img src="${tour.imagePath}${tour.image03}" alt="" />
 </section>
-<section class="trip-plan">
+<section class="trip-plan content-width">
   <section class="trip-plan-item">
     <h2 class="font-size-60 upper title underline">Day #1:</h2>
     <div class="body-text">
@@ -72,5 +40,3 @@ export const tourDetailView = (tour) =>
 </section>
 </section>
 `;
-
-// console.log("test from detailview: ", tourDetailView(requestedtour));
